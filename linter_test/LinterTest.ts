05c2e79ee81106997e0f3e3a0bb7a7eea2b6389a
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-10-23 15:51:14 +0200

    Update LinterTest.ts

--- linter_test/LinterTest.ts
+++ linter_test/LinterTest.ts
@@ -12,10 +12,10 @@ namespace LinterTest {
 
   function DeCrypt(_text: string, _key: number): string {
     let result: string = "";
-    for (let i: number = 0; i <_text.length; i ++){
+    for (let i: number = 0; i < _text.length; i++) {
       result += String.fromCharCode(_text.charCodeAt(i) + _key)
     }
     return result;
-  }  
+  }
 
 }
