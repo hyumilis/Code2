7841f0f1c892ffd3679ea9085d7947a8aa96f723
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-10-27 12:24:56 +0100

    Completed_the_trie_circle

--- Canvas_manipulation/crc.js
+++ Canvas_manipulation/crc.js
@@ -1,15 +1,66 @@
 "use strict";
 var CanvasManipulation;
 (function (CanvasManipulation) {
+    let canvas1;
+    let canvas2;
+    let crc2;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
+        canvas1 = document.getElementById("left");
+        canvas2 = document.getElementById("right");
+        crc2 = canvas1.getContext("2d");
         drawBackground();
+        crc2 = canvas2.getContext("2d");
+        drawtriangles();
     }
     function drawBackground() {
-        let canvas = document.querySelector("canvas");
-        let crc2 = canvas.getContext("2d");
-        crc2.fillStyle = "#4A8DFF";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        setRandomColor();
+        crc2.fillRect(randomwidth(), randomheight(), randomwidth(), randomheight());
+        setRandomColor();
+        crc2.fillText("Hallo Welt", randomwidth(), randomheight(), randomwidth());
+        setRandomColor();
+        crc2.beginPath();
+        crc2.arc(randomwidth(), randomheight(), randomradius(), 0, 2 * Math.PI);
+        crc2.fill();
+    }
+    function drawtriangles() {
+        crc2.fillStyle = "red";
+        const startposx = randomwidth();
+        const startposy = randomheight();
+        const radius = 50;
+        let tries = 0;
+        let validinput = false;
+        while (validinput == false) {
+            const triesinput = prompt("How many triangles do you want the circle to have?(Nubers only)");
+            if (triesinput != null) {
+                const triesnumber = parseInt(triesinput);
+                if (!isNaN(triesnumber) && triesnumber > 2) {
+                    validinput = true;
+                    tries = triesnumber;
+                    break;
+                }
+            }
+        }
+        crc2.beginPath();
+        crc2.moveTo(startposx, startposy);
+        for (let i = 0; i < tries + 1; i++) {
+            crc2.lineTo(startposx + Math.cos(i * 2 * Math.PI / tries) * radius, startposy + Math.sin(i * 2 * Math.PI / tries) * radius);
+        }
+        crc2.stroke();
+        crc2.fill();
+    }
+    function randomwidth() {
+        return Math.round(Math.random() * crc2.canvas.width);
+    }
+    function randomheight() {
+        return Math.round(Math.random() * crc2.canvas.height);
+    }
+    function randomradius() {
+        return Math.round(Math.random() * crc2.canvas.height);
+    }
+    function setRandomColor() {
+        const randcolor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        crc2.fillStyle = randcolor;
     }
 })(CanvasManipulation || (CanvasManipulation = {}));
 //# sourceMappingURL=crc.js.map
--- Canvas_manipulation/crc.ts
+++ Canvas_manipulation/crc.ts
@@ -1,15 +1,68 @@
 namespace CanvasManipulation {
-
+    let canvas1: HTMLCanvasElement;
+    let canvas2: HTMLCanvasElement;
+    let crc2: CanvasRenderingContext2D;
 
     window.addEventListener("load", handleLoad);
     function handleLoad(_event: Event): void {
+        canvas1 = document.getElementById("left") as HTMLCanvasElement;
+        canvas2 = document.getElementById("right") as HTMLCanvasElement;
+        crc2 = canvas1.getContext("2d")!;
         drawBackground();
+        crc2 = canvas2.getContext("2d")!;
+        drawtriangles();
     }
-    
+
+
     function drawBackground(): void {
-        let canvas: HTMLCanvasElement = document.querySelector<HTMLCanvasElement>("canvas")!;
-        let crc2: CanvasRenderingContext2D = canvas.getContext("2d")!;
-        crc2.fillStyle = "#4A8DFF";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        setRandomColor();
+        crc2.fillRect(randomwidth(), randomheight(), randomwidth(), randomheight());
+        setRandomColor();
+        crc2.fillText("Hallo Welt", randomwidth(), randomheight(), randomwidth());
+        setRandomColor();
+        crc2.beginPath();
+        crc2.arc(randomwidth(), randomheight(), randomradius(), 0, 2 * Math.PI);
+        crc2.fill();
+    }
+    function drawtriangles(): void {
+
+        crc2.fillStyle = "red";
+        const startposx: number = randomwidth();
+        const startposy: number = randomheight();
+        const radius: number = 50;
+        let tries: number = 0;
+        let validinput: boolean = false;
+        while (validinput == false) {
+            const triesinput: string | null = prompt("How many triangles do you want the circle to have?(Nubers only)");
+            if (triesinput != null) {
+                const triesnumber: number = parseInt(triesinput);
+                if (!isNaN(triesnumber) && triesnumber > 2) {
+                    validinput = true;
+                    tries = triesnumber;
+                    break;
+                }
+            }
+        }
+        crc2.beginPath();
+        crc2.moveTo(startposx, startposy);
+        for (let i: number = 0; i < tries + 1; i++) {
+            crc2.lineTo(startposx + Math.cos(i * 2 * Math.PI / tries) * radius, startposy + Math.sin(i * 2 * Math.PI / tries) * radius);
+        }
+        crc2.stroke();
+        crc2.fill();
+    }
+
+    function randomwidth(): number {
+        return Math.round(Math.random() * crc2.canvas.width);
+    }
+    function randomheight(): number {
+        return Math.round(Math.random() * crc2.canvas.height);
+    }
+    function randomradius(): number {
+        return Math.round(Math.random() * crc2.canvas.height);
+    }
+    function setRandomColor(): void {
+        const randcolor: string = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        crc2.fillStyle = randcolor;
     }
 }
