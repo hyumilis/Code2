e062c03bfe13ae3db6d61c7b8cc18d4089bcc515
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-10-27 16:46:37 +0100

    added buttens with transform funktions

--- draw_and_transform_coordinat_system/dat.js
+++ draw_and_transform_coordinat_system/dat.js
@@ -3,23 +3,77 @@ var CordSystemManipulation;
 (function (CordSystemManipulation) {
     let canvas;
     let crc;
+    let transformationsaves;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         canvas = document.querySelector("canvas");
         crc = canvas.getContext("2d");
+        crc.fillStyle = "#588ee9ff";
+        crc.fillRect(0, 0, canvas.width, canvas.height);
         drawCoordinateSystem();
-        transformCoordinateSystem();
+        document.getElementById("btnRotate").addEventListener("click", rotate);
+        document.getElementById("btnScaleUp").addEventListener("click", sizeup);
+        document.getElementById("btnScaleDown").addEventListener("click", sizedown);
+        document.getElementById("btnTranslateup").addEventListener("click", moveup);
+        document.getElementById("btnTranslatedown").addEventListener("click", movedown);
+        document.getElementById("btnTranslateLeft").addEventListener("click", moveleft);
+        document.getElementById("btnTranslateRight").addEventListener("click", moveright);
+        document.getElementById("btnReset").addEventListener("click", reset);
     }
     function drawCoordinateSystem() {
-        crc.strokeStyle = "black";
+        crc.strokeStyle = getRandomColor();
+        crc.lineWidth = 1;
         crc.beginPath();
-        crc.moveTo(0, crc.canvas.height / 2);
-        crc.lineTo(crc.canvas.width, crc.canvas.height / 2);
-        crc.moveTo(crc.canvas.width / 2, 0);
-        crc.lineTo(crc.canvas.width / 2, crc.canvas.height);
+        crc.moveTo(0, 0);
+        crc.lineTo(200, 0);
+        crc.stroke();
+        for (let i = 0; i <= 200; i += 10) {
+            crc.moveTo(i, 0);
+            crc.lineTo(i, 5);
+            crc.stroke();
+            crc.moveTo(0, i);
+            crc.lineTo(5, i);
+            crc.stroke();
+        }
+        crc.moveTo(0, 0);
+        crc.lineTo(0, 200);
         crc.stroke();
     }
-    function transformCoordinateSystem() {
+    function rotate() {
+        crc.rotate(20 * Math.PI / 180);
+        drawCoordinateSystem();
+    }
+    function moveup() {
+        crc.translate(0, -20);
+        drawCoordinateSystem();
+    }
+    function moveleft() {
+        crc.translate(-20, 0);
+        drawCoordinateSystem();
+    }
+    function moveright() {
+        crc.translate(20, 0);
+        drawCoordinateSystem();
+    }
+    function movedown() {
+        crc.translate(0, 20);
+        drawCoordinateSystem();
+    }
+    function sizeup() {
+        crc.scale(1.1, 1.1);
+        drawCoordinateSystem();
+    }
+    function sizedown() {
+        crc.scale(0.9, 0.9);
+        drawCoordinateSystem();
+    }
+    function reset() {
+        crc.
+            drawCoordinateSystem();
+    }
+    function getRandomColor() {
+        const randcolor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        return randcolor;
     }
 })(CordSystemManipulation || (CordSystemManipulation = {}));
 //# sourceMappingURL=dat.js.map
--- draw_and_transform_coordinat_system/dat.ts
+++ draw_and_transform_coordinat_system/dat.ts
@@ -1,26 +1,95 @@
 namespace CordSystemManipulation {
     let canvas: HTMLCanvasElement;
     let crc: CanvasRenderingContext2D;
+
+    let transformationsaves: Safes[];
+    interface Safes {
+        translateX: number;
+        translateY: number;
+        rotate: number;
+        scaleX: number;
+        scaleY: number;
+    }
+
     window.addEventListener("load", handleLoad);
     function handleLoad(_event: Event): void {
         canvas = document.querySelector("canvas")!;
         crc = canvas.getContext("2d")!;
+        crc.fillStyle = "#588ee9ff";
+        crc.fillRect(0, 0, canvas.width, canvas.height);
         drawCoordinateSystem();
-        transformCoordinateSystem();
+        document.getElementById("btnRotate")!.addEventListener("click", rotate)!;
+        document.getElementById("btnScaleUp")!.addEventListener("click", sizeup)!;
+        document.getElementById("btnScaleDown")!.addEventListener("click", sizedown)!;
+        document.getElementById("btnTranslateup")!.addEventListener("click", moveup)!;
+        document.getElementById("btnTranslatedown")!.addEventListener("click", movedown)!;
+        document.getElementById("btnTranslateLeft")!.addEventListener("click", moveleft)!;
+        document.getElementById("btnTranslateRight")!.addEventListener("click", moveright)!;
+        document.getElementById("btnReset")!.addEventListener("click", reset)!;
     }
+
+
+
+
+
+
+
+
 
     function drawCoordinateSystem(): void {
-        crc.strokeStyle = "black";
+        crc.strokeStyle = getRandomColor();
+        crc.lineWidth = 1;
         crc.beginPath();
-        crc.moveTo(0, crc.canvas.height / 2);
-        crc.lineTo(crc.canvas.width, crc.canvas.height / 2);
-        crc.moveTo(crc.canvas.width / 2, 0);
-        crc.lineTo(crc.canvas.width / 2, crc.canvas.height);
+        crc.moveTo(0, 0);
+        crc.lineTo(200, 0);
+        crc.stroke();
+        for (let i: number = 0; i <= 200; i += 10) {
+            crc.moveTo(i, 0);
+            crc.lineTo(i, 5);
+            crc.stroke();
+            crc.moveTo(0, i);
+            crc.lineTo(5, i);
+            crc.stroke();
+        }
+        crc.moveTo(0, 0);
+        crc.lineTo(0, 200);
         crc.stroke();
     }
-    function transformCoordinateSystem(): void {
-
+    function rotate(): void {
+        crc.rotate(20 * Math.PI / 180);
+        drawCoordinateSystem();
     }
-
+    function moveup(): void {
+        crc.translate(0, -20);
+        drawCoordinateSystem();
+    }
+    function moveleft(): void {
+        crc.translate(-20, 0);
+        drawCoordinateSystem();
+    }
+    function moveright(): void {
+        crc.translate(20, 0);
+        drawCoordinateSystem();
+    }
+    function movedown(): void {
+        crc.translate(0, 20);
+        drawCoordinateSystem();
+    }
+    function sizeup(): void {
+        crc.scale(1.1, 1.1);
+        drawCoordinateSystem();
+    }
+    function sizedown(): void {
+        crc.scale(0.9, 0.9);
+        drawCoordinateSystem();
+    }
+    function reset(): void {
+        crc.
+            drawCoordinateSystem();
+    }
+    function getRandomColor(): string {
+        const randcolor: string = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        return randcolor;
+    }
 }
 
