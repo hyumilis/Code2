883115453ec6f075d2559179b0ddd5fe3d0c74ff
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-10-27 10:44:19 +0100

    fixed_the_canvas_by_adding_eventlistener

--- Canvas_manipulation/crc.js
+++ Canvas_manipulation/crc.js
@@ -1,9 +1,15 @@
 "use strict";
 var CanvasManipulation;
 (function (CanvasManipulation) {
-    let canvas = document.querySelector("canvas");
-    let crc2 = canvas.getContext("2d");
-    crc2.fillStyle = "#4A8DFF";
-    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        drawBackground();
+    }
+    function drawBackground() {
+        let canvas = document.querySelector("canvas");
+        let crc2 = canvas.getContext("2d");
+        crc2.fillStyle = "#4A8DFF";
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
 })(CanvasManipulation || (CanvasManipulation = {}));
 //# sourceMappingURL=crc.js.map
--- Canvas_manipulation/crc.ts
+++ Canvas_manipulation/crc.ts
@@ -1,8 +1,15 @@
 namespace CanvasManipulation {
 
-let canvas: HTMLCanvasElement = document.querySelector<HTMLCanvasElement>("canvas")!;
-let crc2: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-crc2.fillStyle = "#4A8DFF";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event: Event): void {
+        drawBackground();
+    }
+    
+    function drawBackground(): void {
+        let canvas: HTMLCanvasElement = document.querySelector<HTMLCanvasElement>("canvas")!;
+        let crc2: CanvasRenderingContext2D = canvas.getContext("2d")!;
+        crc2.fillStyle = "#4A8DFF";
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
 }
