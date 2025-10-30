41d896324a567feb58a36930ae8d58bff5968975
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-10-30 15:46:43 +0100

    Added subclasses to animals

--- Old_McDonalds_Farm/Animal.ts
+++ Old_McDonalds_Farm/Animal.ts
@@ -34,5 +34,53 @@ namespace OldMcDonaldsFarm {
                     Old MacDonald had a farm,\n
                     Ee-aye, ee-aye, oh.`;
         }
+        public doSpecialAction(): void {
+            // To be overridden by subclasses
+        }
+    }
+    export class Cow extends Animal {
+        public constructor(_name: string, _hunger: number) {
+            super(_name, Species.Cow, "moo", "grass", _hunger);
+        }
+        public doSpecialAction(): number {
+            console.log(`${this.name} says: Müü Müü Here's some fresh milk!`);
+            return Math.floor(Math.random() * 2 + 1);
+        }
+    }
+    export class Bee extends Animal {
+        public constructor(_name: string, _hunger: number) {
+            super(_name, Species.Bee, "buzz", "nectar", _hunger);
+        }
+        public doSpecialAction(): number {
+            console.log( `${this.name} says: Bzz Bzz! Here's some sweet honey!`);
+            return Math.floor(Math.random() * 5 + 5);
+        }
+    }
+    export class Pig extends Animal {
+        public constructor(_name: string, _hunger: number) {
+            super(_name, Species.Pig, "oink", "slop", _hunger);
+        }
+        public doSpecialAction(): number {
+            console.log( `${this.name} says: Oink oink! Don't eat me, I'm delicious!`);
+            return Math.floor(Math.random() * 6 + 1);
+        }
+    }
+    export class Sheep extends Animal {
+        public constructor(_name: string, _hunger: number) {
+            super(_name, Species.Sheep, "baa", "grass", _hunger);
+        }
+        public doSpecialAction() {
+            console.log( `${this.name} says: Baa baa! Here's some warm wool for you!`);
+            return Math.floor(Math.random() * 2 + 1);
+        }
+    }
+    export class Duck extends Animal {
+        public constructor(_name: string, _hunger: number) {
+            super(_name, Species.Duck, "quack", "seeds", _hunger);
+        }
+        public doSpecialAction(): number {
+            console.log( `${this.name} says: Quack quack! Here's some fresh eggs!`);
+            return Math.floor(Math.random() * 3 + 1);
+        }
     }
 }
