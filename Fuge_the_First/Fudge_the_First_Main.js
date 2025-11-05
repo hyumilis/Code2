d27e0a52742a2f77bc8c639c8e2eb532d61c0f43
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-11-05 12:15:12 +0100

    es

--- Fuge_the_First/Fudge_the_First_Main.js
+++ Fuge_the_First/Fudge_the_First_Main.js
@@ -1,20 +1,38 @@
 "use strict";
-var FudgeCore;
-(function (FudgeCore) {
-    window.addEventListener("load", handleLoad);
-    function handleLoad(_event) {
-        let canvas = document.getElementById("playfield");
+var Fugetry;
+(function (Fugetry) {
+    window.addEventListener("load", hndlload);
+    let canvas;
+    let currentrotation = 0;
+    function hndlload(_event) {
+        canvas = document.getElementById("screen");
         if (!canvas) {
             return;
         }
-        FudgeCore.crc3 = canvas.getContext("2d");
-        FudgeCore.crc3.fillStyle = "black";
-        FudgeCore.crc3.strokeStyle = "white";
-        FudgeCore.crc3.lineWidth = 2;
+        Fugetry.crc3 = canvas.getContext("2d");
+        Fugetry.crc3.fillStyle = "black";
+        Fugetry.crc3.strokeStyle = "white";
+        Fugetry.crc3.lineWidth = 2;
         //canvas.addEventListener("feuerfrei", xxx);
         //canvas.addEventListener("mouseup", xxx);
         //canvas.addEventListener("keypress", xxx);
         //canvas.addEventListener("mousemove", xxx);
+        window.setInterval(start, 20);
     }
-})(FudgeCore || (FudgeCore = {}));
+    function start() {
+        currentrotation += 2;
+        let ƒ = FudgeCore;
+        let mesh = new ƒ.MeshCube("Cube"); // create a simple mesh representing a cube
+        let material = new ƒ.Material("Texture", FudgeCore.ShaderLitTextured); // create a material with the default texture for testing
+        let node = new ƒ.Node("qube"); // create a node as the scene graph
+        node.addComponent(new ƒ.ComponentMesh(mesh)); // refer the mesh to the node
+        node.addComponent(new ƒ.ComponentMaterial(material)); // refer the material to the node
+        node.getComponent(ƒ.ComponentMesh).mtxPivot.rotate(new ƒ.Vector3(-30, currentrotation, -15)); // rotate the mesh
+        let cmpCamera = new ƒ.ComponentCamera(); // setup a camera
+        cmpCamera.mtxPivot.translateZ(-2); // move the camera back
+        let viewport = new ƒ.Viewport(); // create a viewport to manage rendering of the graph to the canvas via the camera
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw(); // render
+    }
+})(Fugetry || (Fugetry = {}));
 //# sourceMappingURL=Fudge_the_First_Main.js.map
--- Fuge_the_First/Fudge_the_First_Main.ts
+++ Fuge_the_First/Fudge_the_First_Main.ts
@@ -1,9 +1,11 @@
 namespace Fugetry {
-    window.addEventListener("load", handleLoad);
+    window.addEventListener("load", hndlload);
     export let crc3: CanvasRenderingContext2D;
+    let canvas: HTMLCanvasElement|null;
+    let currentrotation: number = 0;
 
-    function handleLoad(_event: Event): void {
-        let canvas: HTMLCanvasElement | null = document.getElementById("playfield") as HTMLCanvasElement;
+    function hndlload(_event: Event): void {
+        canvas = document.getElementById("screen") as HTMLCanvasElement;
         if (!canvas) {
             return;
         }
@@ -16,5 +18,27 @@ namespace Fugetry {
         //canvas.addEventListener("mouseup", xxx);
         //canvas.addEventListener("keypress", xxx);
         //canvas.addEventListener("mousemove", xxx);
+        window.setInterval(start, 20);
     }
+    
+    function start():void {
+            currentrotation += 2;
+            let ƒ = FudgeCore;
+            let mesh = new ƒ.MeshCube("Cube"); // create a simple mesh representing a cube
+            let material = new ƒ.Material("Texture", FudgeCore.ShaderLitTextured); // create a material with the default texture for testing
+
+            let node = new ƒ.Node("qube"); // create a node as the scene graph
+            node.addComponent(new ƒ.ComponentMesh(mesh)); // refer the mesh to the node
+            node.addComponent(new ƒ.ComponentMaterial(material)); // refer the material to the node
+
+            node.getComponent(ƒ.ComponentMesh).mtxPivot.rotate(new ƒ.Vector3(-30, currentrotation, -15)); // rotate the mesh
+
+            let cmpCamera = new ƒ.ComponentCamera(); // setup a camera
+            cmpCamera.mtxPivot.translateZ(-2); // move the camera back
+
+            let viewport = new ƒ.Viewport(); // create a viewport to manage rendering of the graph to the canvas via the camera
+            viewport.initialize("Viewport", node, cmpCamera, canvas!);
+
+            viewport.draw(); // render
+        }
 }
